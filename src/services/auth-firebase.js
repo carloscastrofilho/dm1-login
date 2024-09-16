83115b85430e22ab6b52f767ab7cc10f021670bf
Carlos Pereira de Castro Filho <50716871+carloscastrofilho@users.noreply.github.com>
2024-09-16 14:09:14 -0300

    limpando chaves firebase

--- src/services/auth-firebase.js
+++ src/services/auth-firebase.js
@@ -1,27 +1,43 @@
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../services/database/firebase'
+import {
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../services/database/firebase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export async function LoginUsuario() {
-    const usuario =  await signInWithEmailAndPassword(auth, "teste@gmail.com", "123456789");
-    console.log(usuario)
-    return usuario;
+	const usuario = await signInWithEmailAndPassword(
+		auth,
+		"teste@gmail.com",
+		"123456789",
+	);
+	console.log(usuario);
+	return usuario;
 }
 
 export async function LogoutUsuaro() {
-    auth.signOut()
+	auth.signOut();
+	// Limpa todo o AsyncStorage
+	AsyncStorage.clear()
+		.then(() => {
+			console.log("AsyncStorage limpo com sucesso!");
+		})
+		.catch((error) => {
+			console.error("Erro ao limpar o AsyncStorage:", error);
+		});
 }
 
-export async function RegistarUsuario(){
-   await createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-    console.log(user);
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+export async function RegistarUsuario() {
+	await createUserWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+			// ...
+			console.log(user);
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// ..
+		});
 }
